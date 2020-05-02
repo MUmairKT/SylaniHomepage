f2f4d39d9f6eb82061ee8fdcbaa6d40d92b36c56
M.umairkt <m.umairkt@gmail.com>
2020-05-03 00:05:56 +0500

    Assignment 3 sylani website homepage first commit

---
+++ SylaniWebsite/js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $(".nav-item>a").on({
+        mouseover: function () {
+            $(this).addClass('btn-primary text-white');
+            
+        },
+        mouseleave: function () {
+            $(this).removeClass('btn-primary text-white');
+        }               
+    });
+    
+
+});
+
